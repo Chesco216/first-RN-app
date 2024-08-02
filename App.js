e92bbb6c64b6a18fb163bbd2f222d1896117cac3
Chesco216 <franchescofv198@gmail.com>
2024-08-01 23:37:30 -0400

    i just wanna be yours

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native';
 
 export default function App() {
+  
+  const [value, setValue] = useState('')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>PIPIPI</Text>
+      <Image
+        src='https://pbs.twimg.com/media/E4btIa3WUAIEpDW.jpg'
+        style={styles.image}
+      />
+      <TextInput
+        placeholder='who is she?'
+        onChangeText={text => setValue(text)}
+      />
+      <Text>
+        {value}
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +32,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
   },
 });
